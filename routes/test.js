83ffa365656eee9e355e123a1715d4d40be5c8e5
Farid Khosravi <farid.khosravi@hotmail.com>
2017-06-11 01:49:21 +0300

    first commit

---
+++ routes/test.js
@@ -0,0 +1,3 @@
+//var json =  "order xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance" "xsi:noNamespaceSchemaLocation="users.xsd"\";
+var json = "order xmlns:xsi = http://www.w3.org/2001/XMLSchema-instance xsi:noNamespaceSchemaLocation=users.xsd";
+console.log(json);
